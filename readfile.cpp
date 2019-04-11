bed7acb7f46a69585c7a93d5e20cc9993252c47c
yuanmathcode <liaoyuan.cheng95@gmail.com>
2019-04-11 15:17:30 +0200

    Add files via upload

--- readfile.cpp
+++ readfile.cpp
@@ -27,18 +27,18 @@ public:
 class edge{
 private:
 	int _id;
-	pair<shared_ptr<node>,shared_ptr<node>> _origintoend;
+//	pair<shared_ptr<node>,shared_ptr<node>> _origintoend;
 	shared_ptr<node> _origin;
 	shared_ptr<node> _end;
 	double _length;
 public:
 	edge()=default;
-	edge(shared_ptr<node> given_origin,shared_ptr<node> given_end, double given_length)
-	:_origin(given_origin),_end(given_end),_length(given_length){}
+	edge(int given_id,shared_ptr<node> given_origin,shared_ptr<node> given_end, double given_length)
+	:_id(given_id),_origin(given_origin),_end(given_end),_length(given_length){}
 	int &id() {return _id;}
-	pair<shared_ptr<node>,shared_ptr<node>> &origintoend() {return _origintoend;}
-	const shared_ptr<node> &origin() {return _origin;}
-	const shared_ptr<node> &end() {return _end;}
+//	pair<shared_ptr<node>,shared_ptr<node>> &origintoend() {return _origintoend;}
+	shared_ptr<node> &origin() {return _origin;}
+	shared_ptr<node> &end() {return _end;}
 	double &length() {return _length;}
         void define_id(int g_id){
                 _id=g_id;}
@@ -51,18 +51,36 @@ public:
 
 };
 
-/*class cchannel{
+class cchannel{
 private:
 	int _id;
-	set<shared_ptr<node>> _ntoch;
+	set<shared_ptr<node>> _vals;
 public:
-	const int id() {return _id;}
-	set<shared_ptr<node>> &ntoch() {return _ntoch;}
-};*/
-vector<shared_ptr<node>> vecnode;
-vector<shared_ptr<edge>> vecedge;
+	cchannel()=default;
+	cchannel(int id)
+	:_id(id){}
+	const int &id() {return _id;}
+	set<shared_ptr<node>> &vals() {return _vals;}
+	void add_val(shared_ptr<node> val){
+		if(val->type()=='v')
+			_vals.insert(val);
+		else
+			cerr<<"Error:only valve are allowed to be added to cchannel!"<<endl;
+	}
+};
+
+class Graph{
+public:
+	vector<shared_ptr<node>> vecnode;
+	vector<shared_ptr<edge>> vecedge;
+	vector<set<shared_ptr<node>>> vecchan;
+	Graph()=default;
+};
+//vector<shared_ptr<node>> vecnode;
+//vector<shared_ptr<edge>> vecedge;
 
 void readfile(const char* filename){
+	Graph g;
 	ifstream read(filename);
 	string str_temp;
 	int int_temp,int_temp1,int_temp2;
@@ -75,7 +93,7 @@ void readfile(const char* filename){
 					break;
 			int_temp=stoi(str_temp);
 			shared_ptr<node> node_temp=make_shared<node>(int_temp,'i');
-			vecnode.push_back(node_temp);
+			g.vecnode.push_back(node_temp);
 			}
 		}
 		//read outlet
@@ -85,7 +103,7 @@ void readfile(const char* filename){
                                         break;
                         int_temp=stoi(str_temp);
                         shared_ptr<node> node_temp=make_shared<node>(int_temp,'o');
-                        vecnode.push_back(node_temp);
+                        g.vecnode.push_back(node_temp);
                         }
                 }
 		//read branch
@@ -95,7 +113,7 @@ void readfile(const char* filename){
                                         break;
                         int_temp=stoi(str_temp);
                         shared_ptr<node> node_temp=make_shared<node>(int_temp,'b');
-                        vecnode.push_back(node_temp);
+                        g.vecnode.push_back(node_temp);
                         }
                 }
 		//read valve
@@ -105,39 +123,58 @@ void readfile(const char* filename){
                                         break;
                         int_temp=stoi(str_temp);
                         shared_ptr<node> node_temp=make_shared<node>(int_temp,'v');
-                        vecnode.push_back(node_temp);
+                        g.vecnode.push_back(node_temp);
                         }
                 }
+		//Read edge
 		if(str_temp=="Edge:"){
-	//		int index_temp=0;
+			int index_temp=0;
 			while(getline(read,str_temp)){
 				if(str_temp=="end")
 					break;
 				if(str_temp.empty())
 					continue;
-	//		index_temp++;
+			index_temp++;
 			istringstream edge_record(str_temp);
 			edge_record>>int_temp1>>int_temp2>>double_temp;
 			shared_ptr<node> node_temp1, node_temp2;
-			for(auto &i:vecnode){
+			for(auto &i:g.vecnode){
 				if(i->id()==int_temp1)
 					node_temp1=i;
 			}
-			for(auto &j:vecnode){
+			for(auto &j:g.vecnode){
 				if(j->id()==int_temp2)
 					node_temp2=j;
 			}
-			shared_ptr<edge> edge_temp=make_shared<edge>(node_temp1,node_temp2,double_temp);
-			vecedge.push_back(edge_temp);
+			shared_ptr<edge> edge_temp=make_shared<edge>(index_temp,node_temp1,node_temp2,double_temp);
+			g.vecedge.push_back(edge_temp);
 			}
-
+		}
+		//Read Channel
+		if(str_temp=="CChannel:"){
+			int id_temp=0;
+			int v_id_temp=0;
+			while(getline(read,str_temp){
+				if(str_temp=="end")
+					break;
+				if(str_temp.empty())
+					continue;
+				istringstream cch_record(str_temp);
+				if(cch_record>>id_temp){
+					shared_ptr<cchannel> chan_temp=make_shared<cchannel>(id_temp);
+					while(cch_record>>v_id_temp)
+						chan_temp->val().add
+			}
 		}
 	}
+	for(auto &j:g.vecnode)
+		cout<<j->id()<<endl;
+//	for(auto &i:g.vecedge)
+//		cout<<i->length()<<endl;
+
 }
 int main(){
 	readfile("design.txt");
-	for(int i=0;i<vecedge.size();i++)
-		cout<<vecedge[i]->length()<<" "<<endl;
 }
 
 
